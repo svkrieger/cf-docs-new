2f1398b6b4f6bcd19113edd778c0d7f1a5fd2887
Sven Krieger <37476281+svkrieger@users.noreply.github.com>
2025-04-08 15:14:42 +0200

    WIP

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,24 +8,28 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
+const host = 'https://svkrieger.github.io'
+const path = '/cf-docs-new/'
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Cloud Foundry Documentation',
+  tagline: 'Documentation to help you deploy and manage Cloud Foundry',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: host,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: path,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'svkrieger', // Usually your GitHub org/user name.
+  projectName: 'cf-docs-new', // Usually your repo name.
+  trailingSlash: false,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
@@ -36,17 +40,25 @@ const config = {
     locales: ['en'],
   },
 
+  staticDirectories: [
+    'static',
+    'content_repos/docs-cloudfoundry-concepts/static'
+  ],
+
   presets: [
     [
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
+          path: 'content_repos/docs-cloudfoundry-concepts',
+          routeBasePath: 'docs-cloudfoundry-concepts',
+          sidebarPath: './sidebars-docs-cloudfoundry-concepts.ts',
+          showLastUpdateTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/cloudfoundry/docs-cloudfoundry-concepts/tree/master/',
         },
         blog: {
           showReadingTime: true,
@@ -70,58 +82,69 @@ const config = {
     ],
   ],
 
+  plugins: [
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'docs-dev-guide',
+        path: 'content_repos/docs-dev-guide',
+        routeBasePath: 'docs-dev-guide',
+        sidebarPath: './sidebars-docs-dev-guide.ts',
+        showLastUpdateTime: true,
+      },
+    ],
+  ],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Cloud Foundry Documentation',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Cloud Foundry Logo',
+          src: 'img/logo.png',
         },
         items: [
+          {to: '/docs-cloudfoundry-concepts/cloudfoundry-concepts', label: 'Cloud Foundry Concepts', position: 'left'},
+          {to: '/docs-dev-guide', label: 'Dev Guide', position: 'left'},
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/cloudfoundry',
             label: 'GitHub',
             position: 'right',
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: 'light',
         links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
           {
             title: 'Community',
             items: [
               {
+                label: 'Community Website',
+                href: 'https://www.cloudfoundry.org/community/',
+              },
+              {
+                label: 'Slack',
+                href: 'https://slack.cloudfoundry.org/',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/cloudfoundry/',
+              },
+              {
+                label: 'YouTube',
+                href: 'https://www.youtube.com/channel/UC0ZYS0Y7b5oiVLvxGf4magw',
+              },
+              {
                 label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                href: 'https://stackoverflow.com/questions/tagged/cloud-foundry?tab=Newest',
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
+                label: 'Mailing List',
+                href: 'https://lists.cloudfoundry.org/g/announce',
               },
             ],
           },
@@ -134,12 +157,12 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/cloudfoundry',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `<a href='${host}${path}'>Cloud Foundry Documentation<a/> © ${new Date().getFullYear()} <a href='https://cloudfoundry.org'>Cloud Foundry Foundation<a/>. All Rights Reserved.`,
       },
       prism: {
         theme: prismThemes.github,
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -51,14 +51,12 @@ function Feature({Svg, title, description}) {
 
 export default function HomepageFeatures() {
   return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
+    <div className={styles.landing}>
+      <div className={styles.centeredContent}>
+          <Heading as="h1">Getting Started with</Heading>
+          <img className={styles.logoSvg} role="img" src="img/new_logo_org.png" alt="Cloud Foundry Logo" />
+          <Heading as="h2">Cloud Foundry is an open-source platform as a service (PaaS) that provides you with a choice of clouds, developer frameworks, and application services.</Heading>
       </div>
-    </section>
+    </div>
   );
 }
